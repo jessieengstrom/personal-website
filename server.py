8c978e6c50f1f528d8998bf31841c7929c7e20b5
Jessie Engstrom <jessielengstrom@gmail.com>
2018-04-25 20:15:06 -0700

    Added a background photo and a about me section.

--- server.py
+++ server.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return '<h1>Hello</h1>'
+    """Render the homepage."""
+    return render_template('index.html')
 
 
 if __name__ == "__main__":
